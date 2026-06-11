466e37b5ccb40b66da935e7ce75845ed3c4d58bb
Dineth De Alwis <dinethdilhara66@gmail.com>
2026-06-11 11:44:00 +0530

    feat(docs): add system-design-challenge blog and diagram

--- lib/diagrams.ts
+++ lib/diagrams.ts
@@ -1,7 +1,9 @@
 import testJson from "../diagrams/test.json";
+import systemDesignJson from "../diagrams/system-design-challenge.json";
 
 export const diagrams = {
   "test": testJson,
+  "system-design-challenge": systemDesignJson,
 } as const;
 
 export type DiagramName = keyof typeof diagrams;
